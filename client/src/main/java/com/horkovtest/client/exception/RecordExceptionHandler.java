060293cbe58eb737f74cc597d7d39b5437a2c5c3
LUXOFT\MHorkov <mhorkov@luxoft.com>
2022-11-29 12:25:21 +0100

    RecordExceptionHandler added.

---
+++ client/src/main/java/com/horkovtest/client/exception/RecordExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.horkovtest.client.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@Slf4j
+public class RecordExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NoRecordExistsException.class)
+    protected ResponseEntity<String> handleNoRecordExistsException(NoRecordExistsException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+}
