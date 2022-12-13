620d277c265ecdf15eae5e70a59b3ff313ccf6d8
LUXOFT\MHorkov <mhorkov@luxoft.com>
2022-12-13 13:39:25 +0100

    consumes parameter added in registerRecord method.

--- client/src/main/java/com/horkovtest/client/ClientController.java
+++ client/src/main/java/com/horkovtest/client/ClientController.java
@@ -4,6 +4,7 @@ import com.horkovtest.client.exception.NoRecordExistsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +23,7 @@ public class ClientController {
 
     /**
      * GET request in order to obtain {@linkplain Record} by given Id.
+     *
      * @param id String Primary_key of the {@linkplain Record}.
      * @return {@linkplain Record}
      * @throws NoRecordExistsException in case when {@linkplain Record} wasn't found.
@@ -35,10 +37,11 @@ public class ClientController {
     /**
      * In case of empty file or file format other than "text/csv" returns {@linkplain HttpStatus} 400. Otherwise,
      * {@linkplain HttpStatus} 202 returned with information how many rows were saved.
+     *
      * @param file received from client.
      * @return {@linkplain ResponseEntity} with String message of result.
      */
-    @PostMapping(path = "/upload_csv")
+    @PostMapping(path = "/upload_csv", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> registerRecord(@RequestParam(value = "file") MultipartFile file) {
         if (file.isEmpty() || file.getContentType() == null || !file.getContentType().equals("text/csv")) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please select a CSV file to upload.");
